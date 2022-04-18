c42e01c7f89532e154e1e1a3d2c60bdac6c32490
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-04-18 11:08:19 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    music.playMelody("C5 B C5 G C5 B C5 E ", 120)
+})
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+})
